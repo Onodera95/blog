fe92d7d51df5449feabdfea827bee172126e6334
Onodera95 <uchkalec1995@mail.ru>
2017-10-31 20:48:41 +0300

    Подключение БД

--- index.php
+++ index.php
@@ -4,38 +4,56 @@
     <meta charset='utf-8'>
 </head>
 <body>
+<div>
+    <table class="table" border="1px">
+        <tbody>
+        <tr>
 
-<br/>
-<br/>
 
-<?php
+        <?php
 
-include_once('DB.php');
+        include_once('DB.php');
 
-// подключаемся к серверу
-$link = mysqli_connect($db_host, $db_user, $db_password, $db_name)
-or die("Ошибка " . mysqli_error($link));
-if($link)
-{
-    echo "Подключено";
-}
+        // подключаемся к серверу
+        $link = mysqli_connect($db_host, $db_user, $db_password, $db_name)
+        or die("Ошибка " . mysqli_error($link));
+        if($link)
+        {
+            echo "Подключено";
+        }
 
-echo '<br>';
+        echo '<br>';
 
-// выполняем операции с базой данных
-$query ="SELECT * FROM category";
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-if($result)
-{
-    echo "Выполнение запроса прошло успешно";
-}
+        // выполняем операции с базой данных
+        $query ="SELECT * FROM category";
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+        if($result)
+        {
+            echo "Выполнение запроса прошло успешно";
+        }
+        echo "<br>";
+        echo "<br>";
 
-// закрываем подключение
-mysqli_close($link);
+        // Цикл по recordset $result
+        // Каждый ряд становится массивом ($row) с помощью функции mysql_fetch_array
+        while($row = mysqli_fetch_array($result)) {
 
-?>
+            // Записать значение столбца FirstName (который является теперь массивом $row)
+            echo $row['id'] . ". ";
+            echo $row['name'] . "<br />";
 
-<br/>
-<br/>
+            echo '<td>'. $row['name'] . '</td>';
+
+        }
+
+        // закрываем подключение
+        mysqli_close($link);
+
+        ?>
+        </tr>
+        </tbody>
+    </table>
+
+</div>
 </body>
 </html>
