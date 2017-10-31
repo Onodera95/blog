23ebea6aa58fd54fe3da30843f97627c526bba85
Onodera95 <uchkalec1995@mail.ru>
2017-10-31 16:46:42 +0300

    Подключение БД

--- DB.php
+++ DB.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_host = '127.0.0.1';
+$db_host = '91.219.101.186:33066';
 $db_user = 'root';
-$db_password = '';
-$db_name = 'id3197912_blog';
+$db_password = 'root1';
+$db_name = 'blog';
